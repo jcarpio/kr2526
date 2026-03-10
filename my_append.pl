3b2738f7435c6c00cf91c4deb665bbfd29f49eb2
José <jose.carpio@gmail.com>
2026-03-10 14:33:28 +0100

    Update my_append.pl

--- my_append.pl
+++ my_append.pl
@@ -4,3 +4,6 @@ my_append(?List1, ?List2, ?R)
   is true if R is the concatenation of List1 and List2.
   
 */
+
+my_append([], L2, L2).
+my_append([Head|Tail], L2, [Head|R]):- my_append(Tail, L2, R).
