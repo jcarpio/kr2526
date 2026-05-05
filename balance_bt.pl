ac493575427e662151f815ae8922748ccc08f163
José <jose.carpio@gmail.com>
2026-05-05 13:48:02 +0200

    Add files via upload

---
+++ balance_bt.pl
@@ -0,0 +1,53 @@
+
+
+/*
+
+create_bt(+List, -Tree)
+  is true if Tree unify with a binary tree will all labels in List.
+  Tree have to be balanced.
+
+*/
+
+
+create_bt
+
+
+
+
+
+
+
+/*
+
+balanced(+Tree)
+  is true if for all nodes in Tree the difference
+  between left and right tall is maximum 1 in absolute value.
+ 
+*/
+
+
+balanced(nil).
+
+balanced(t(_, Left, Right)):-
+  tree_tall(Left, Left_tall),
+  tree_tall(Right, Right_tall),
+  Diff is Left_tall - Right_tall,
+  Abs is abs(Diff), Abs =< 1,
+  balanced(Left), balanced(Right).
+ 
+/* 
+tree_tall(+Tree, -Tall)
+ is true if Tall unify with Tree's tall.
+
+*/
+
+tree_tall(nil, 0).
+
+tree_tall( t(_, Left, Right), T):-
+  tree_tall(Left, Tall_left),
+  tree_tall(Right, Tall_right),
+  T is max(Tall_left, Tall_right) + 1. 
+ 
+
+
+
